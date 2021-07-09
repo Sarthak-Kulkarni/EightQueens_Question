f091eee690d7c0880df9b8cc1c798e623eebf547
Sarthak-Kulkarni <sarthak3050@gmail.com>
2021-07-09 13:48:24 +0530

    Solved the problem

--- EightQueens_Question/src/GameStrategy.java
+++ EightQueens_Question/src/GameStrategy.java
@@ -4,14 +4,16 @@ public class GameStrategy {
 
 	private int getColumn(int cellId) {
 		// WRITE YOUR LOGIC HERE...................................		
-
-		return 0;	// just for the heck of it
+		
+		int column=cellId%8;
+		return column;	
 	}
 	
 	private int getRow(int cellId) {
 		// WRITE YOUR LOGIC HERE....................................
 		
-		return 0;	// just for the heck of it
+		int row=(int)cellId/8;
+		return row;	
 	}
 
 	public boolean isValidPosition(int cellId) {
@@ -29,6 +31,44 @@ public class GameStrategy {
 			WRITE YOUR LOGIC HERE...............................
 
 		*/
+
+		//Row 
+		for(int i=0;i<8;i++)
+			if(placedQueens[row][i])
+				isValid=false;
+
+		//Column
+		for(int i=0;i<8;i++)
+			if(placedQueens[i][col])
+				isValid=false;
+		
+		// Upper Right
+	    for (int i=row,j=col; i>=0 && j>=0; i--, j--)
+	        if (placedQueens[i][j])
+	            isValid= false;
+	  
+	    // Lower Right
+	    for (int i=row, j=col; j>=0 && i<8; i++, j--)
+	        if (placedQueens[i][j])
+	            isValid= false;
+	    
+	    // Upper Left 
+	    for (int i=row,j=col; i<8 && j<8; i++, j++)
+	        if (placedQueens[i][j])
+	            isValid= false;
+	    
+	    // Lower Left 
+	    for (int i=row, j=col; j<8 && i>=0; i--, j++)
+	        if (placedQueens[i][j])
+	            isValid= false;
+	  
+	    
+	    if(isValid)
+			placedQueens[row][col]=true;
+	    
+//	    System.out.println(row+" "+col);
+//	    System.out.println(placedQueens[row][col]+" "+isValid);
+		
 		return isValid;
 	}
 }
